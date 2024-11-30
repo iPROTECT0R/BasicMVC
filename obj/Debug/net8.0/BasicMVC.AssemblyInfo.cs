f5d2861b0820a30390c75971bfa9acf34ec2db8a
Ray.Shelton <rshelt2@live.spcollege.edu>
2024-11-29 14:41:02 -1000

    Changed index file

--- obj/Debug/net8.0/BasicMVC.AssemblyInfo.cs
+++ obj/Debug/net8.0/BasicMVC.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BasicMVC")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+59cb0f7a315f06b26f10141d3e6563b36a9a053c")]
 [assembly: System.Reflection.AssemblyProductAttribute("BasicMVC")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BasicMVC")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
